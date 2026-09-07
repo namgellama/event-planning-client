0566295d6643f20c4826d259430a47429b89e713
namgel <namgeltamang14@gmail.com>
2026-09-07 12:26:51 +0545

    Configure tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
@@ -5,11 +7,16 @@ import { Toaster } from "sonner";
 import App from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-            <Toaster />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+                <Toaster />
+            </BrowserRouter>
+            <ReactQueryDevtools />
+        </QueryClientProvider>
     </StrictMode>,
 );
