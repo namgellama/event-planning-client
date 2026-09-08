3eb344b5f2e40f9fff0391e585e476e5e4fe643f
namgel <namgeltamang14@gmail.com>
2026-09-08 11:32:09 +0545

    Build form select component

---
+++ src/components/shared/form-select.tsx
@@ -0,0 +1,71 @@
+import {
+    Controller,
+    type FieldPath,
+    type FieldValues,
+    type UseFormReturn,
+} from "react-hook-form";
+import { Field, FieldContent, FieldError, FieldLabel } from "../ui/field";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "../ui/select";
+
+interface Props<T extends FieldValues> {
+    form: UseFormReturn<T>;
+    name: FieldPath<T>;
+    label: string;
+    data: { label: string; value: string }[];
+}
+
+const FormSelect = <T extends FieldValues>({
+    form,
+    name,
+    label,
+    data,
+}: Props<T>) => {
+    return (
+        <Controller
+            name={name}
+            control={form.control}
+            render={({ field, fieldState }) => (
+                <Field
+                    orientation="responsive"
+                    data-invalid={fieldState.invalid}
+                >
+                    <FieldContent>
+                        <FieldLabel htmlFor={field.name}>{label}</FieldLabel>
+
+                        {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                        )}
+                    </FieldContent>
+                    <Select
+                        name={field.name}
+                        value={field.value}
+                        onValueChange={field.onChange}
+                    >
+                        <SelectTrigger
+                            id={field.name}
+                            aria-invalid={fieldState.invalid}
+                            className="min-w-30"
+                        >
+                            <SelectValue placeholder="Select" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {data.map(({ label, value }) => (
+                                <SelectItem key={value} value={value}>
+                                    {label}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </Field>
+            )}
+        />
+    );
+};
+
+export default FormSelect;
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -3,5 +3,6 @@ export { default as DeleteAlertDialog } from "./delete-alert-dialog";
 export { default as EmptyState } from "./empty-state";
 export { default as ErrorState } from "./error-state";
 export { default as FormInput, FormPasswordInput } from "./form-input";
+export { default as FormSelect } from "./form-select";
 export { default as NotFoundState } from "./not-found-state";
 export { default as StatusState } from "./status-state";
